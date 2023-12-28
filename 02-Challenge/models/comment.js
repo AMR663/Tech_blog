69ee0624f410c381b5fc443c9f40078018b4c09c
Adrian Randoll <randolla66@gmail.com>
2023-12-27 21:51:13 -0800

    all of the modles have been created

---
+++ 02-Challenge/models/comment.js
@@ -0,0 +1,45 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class Comment extends Model { }
+
+Comment.init(
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog',
+        key: 'id',
+      },
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isEdited: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
